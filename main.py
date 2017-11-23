17bbc3076946ae316504e167a7e32e347855599a
arfullight <tmimvhuang@gmail.com>
2017-11-23 12:15:14 -0800

    refactoring

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 from data_process import process_train_data
 
 frame = pd.read_csv("data/train.csv")
 features_train, features_test, labels_train, labels_test = process_train_data(frame)
 
-# first_row = features_train.loc[0].values.reshape((28,28))
-
-# print np.array2string(first_row, max_line_width=np.inf)
-# print features_test
-
-print ("start training")
+print("start training")
 clf = RandomForestClassifier(n_estimators=100)
 clf.fit(features_train, labels_train)
 
-print ([clf.predict(features_test[0:1]), labels_test[0:1]])
+# print prediction of the first test data
+print(clf.predict(features_test[0:1, :]))
 
-print ("start calculate score")
-print (clf.score(features_test, labels_test))
+# print label of the first test data
+print(labels_test[0:1])
+
+print("start calculate score")
+print(clf.score(features_test, labels_test))
